331dce3ae2cb9253284004731e2bcf0b63bef695
erto coskuner <erto_coskuner@hotmail.com>
2021-06-19 15:45:07 +0300

    second commit

--- src/booleans.js
+++ src/booleans.js
@@ -1,61 +1,122 @@
 const negate = a => {
-  // your code here
+  return (!a > 0)
 };
 
 const both = (a, b) => {
-  // your code here
+ 
+  return (a && b);
+  
 };
 
 const either = (a, b) => {
-  // your code here
+  if (a || b) {
+    return true
+  } else {
+    return false
+  };
 };
 
 const none = (a, b) => {
-  // your code here
+  if (!a && !b) {
+    return true
+  } else {
+    return false
+  }
 };
-
 const one = (a, b) => {
-  // your code here
+if (a === !b){
+  return true
+} else {
+  return false;
 };
 
+};
 const truthiness = a => {
-  // your code here
+  if (a) {
+    return true
+  } else {
+    return false
+  }
 };
 
 const isEqual = (a, b) => {
-  // your code here
+ if (a === b) {
+   return true
+ } else {
+   return false
+ }
 };
 
 const isGreaterThan = (a, b) => {
-  // your code here
+  if (a > b) {
+    return true
+  } else {
+    return false
+  };
 };
 
 const isLessThanOrEqualTo = (a, b) => {
-  // your code here
+ if (a <= b) {
+   return true
+ } else {
+   return false
+ };
 };
 
 const isOdd = a => {
-  // your code here
+  if (a % 2 == 0) {
+    return false;
+  } else {
+    return true;
+  };
+
 };
 
 const isEven = a => {
-  // your code here
+  
+   if (a % 2 == 0) {
+    return true;
+  } else {
+    return false;
+  };
 };
 
 const isSquare = a => {
-  // your code here
+if (Number.isInteger(Math.sqrt(a))) {
+  return true
+} else {
+  return false
+}
+
 };
 
 const startsWith = (char, string) => {
-  // your code here
+  
+  if (string.charAt(0) === char) {
+    return true
+  } else  {
+    return false
+  }
+
 };
 
 const containsVowels = string => {
-  // your code here
+  
+  string = string.toLowerCase(); 
+  if (string.includes("a") || string.includes("o")) {
+    return true
+  } else {
+    return false
+  }
 };
 
 const isLowerCase = string => {
-  // your code here
+    
+ if (string === string.toLowerCase()) {
+   return true
+ } else {
+   return false
+ }
 };
 
 module.exports = {
